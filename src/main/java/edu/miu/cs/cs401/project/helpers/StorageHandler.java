162a2d94595e0493bfe2db88b13d3402a6135abf
TungTop <stefanitvn@gmail.com>
2020-12-03 11:14:03 -0600

    up

--- src/main/java/edu/miu/cs/cs401/project/helpers/StorageHandler.java
+++ src/main/java/edu/miu/cs/cs401/project/helpers/StorageHandler.java
@@ -141,7 +141,7 @@ public class StorageHandler {
     	flights.removeAll(flights);
     }
 
-    public static FlightNumber createRandomFlight(int amount) {
+    public static FlightNumber createRandomFlightNumber(int amount) {
         int number = (int)Math.random();
         int capacity = amount;
         Airline airlineOwn = new Airline("AIA", "American Airline", "history");
@@ -149,7 +149,11 @@ public class StorageHandler {
         Airport arrivalAirport = getRandomAirport();
         Date departureTime = new Date();
         Date arrivalTime = new Date();
-        return new FlightNumber(number, capacity, airlineOwn, departureAirport, arrivalAirport, departureTime, arrivalTime);
+
+        FlightNumber fn = new FlightNumber(number, capacity, airlineOwn, departureAirport, arrivalAirport, departureTime, arrivalTime);
+        flightNumbers.add(fn);
+
+        return fn;
     }
     
     public static void createRandomFlightNumbers(int amount, Airport departureAirport, Airport arrivalAirport) {
@@ -217,7 +221,7 @@ public class StorageHandler {
         List<String> crews = createRandomCrew(5);
 
         return new Flight(
-                createRandomFlight(5),
+                createRandomFlightNumber(5),
                 dateFlight,
                 passengers,
                 crews,
@@ -266,8 +270,10 @@ public class StorageHandler {
         StorageHandler.createRandomAirports(10);
         StorageHandler.getRandomAirline();
 
-        //TODO:
-
+        for (Airport a: airports) {
+            a.addArrivalFlights( createRandomFlightNumber(3));
+            a.addDepartureFlight( createRandomFlightNumber(2));
+        }
     }
 
     public static void printReservations() {
